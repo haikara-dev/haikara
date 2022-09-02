66e471da72c994220e2a4adac8bfdde26c826e09
cubdesign <takeo@cubdesign.com>
2022-09-03 00:50:56 +0900

    deleted unnecessary files

---
+++ front/src/pages/index.tsx
@@ -0,0 +1,33 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>dailyfj</title>
+        <meta name="description" content="dailyfj" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>dailyfj</h1>
+
+
+      </main>
+
+      <footer>
+        <a
+          href="https://cubdesign.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ©︎cubdesign
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
