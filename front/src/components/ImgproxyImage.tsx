442a6129682a06ce085f631e649b3afbdd24c87a
cubdesign <takeo@cubdesign.com>
2022-10-19 21:10:03 +0900

    add IMGPROXY URL ENV

--- front/src/components/ImgproxyImage.tsx
+++ front/src/components/ImgproxyImage.tsx
@@ -2,9 +2,11 @@ import base64url from "base64url";
 import Image, { ImageLoaderProps, ImageProps } from "next/image";
 import { FC } from "react";
 
+const IMGPROXY_URL: string = process.env.NEXT_PUBLIC_IMGPROXY_URL!;
+
 const ImgproxyLoader = ({ src, width, quality }: ImageLoaderProps) => {
   const url = base64url(src);
-  return `http://localhost:8087/sig/w:${width}/q:${quality || 80}/${url}`;
+  return `${IMGPROXY_URL}/sig/w:${width}/q:${quality || 80}/${url}`;
 };
 
 const ImgproxyImage: FC<ImageProps> = (props) => {
