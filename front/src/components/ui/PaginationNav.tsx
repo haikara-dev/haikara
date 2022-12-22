e0708d5d14d690c886549cfc50935cb6ff31a39b
cubdesign <takeo@cubdesign.com>
2022-12-22 17:18:26 +0900

    add pagenation

--- front/src/components/ui/PaginationNav.tsx
+++ front/src/components/ui/PaginationNav.tsx
@@ -1,7 +1,8 @@
 import ChevronLeftIcon from "@heroicons/react/20/solid/ChevronLeftIcon";
 import ChevronRightIcon from "@heroicons/react/20/solid/ChevronRightIcon";
-import { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import PaginationNavButton from "@/components/ui/PaginationNavButton";
+import { c, w } from "msw/lib/SetupApi-75fbec12";
 
 type PaginationNavProps = {
   page: number;
@@ -9,9 +10,104 @@ type PaginationNavProps = {
   onChange: (e: React.ChangeEvent<unknown>, page: number) => void;
 };
 
-const buttonLength = 7;
+type Page = {
+  label: string;
+  current: boolean;
+  data?: number;
+};
+
+const BUTTON_LENGTH = 7;
 
 const PaginationNav: FC<PaginationNavProps> = ({ page, count, onChange }) => {
+  const [pages, setPages] = useState<Page[]>([]);
+
+  const onClickHandler = (e: React.ChangeEvent<unknown>, page: number) => {
+    console.log("page", page);
+    onChange(e, page);
+  };
+
+  useEffect(() => {
+    const nextPages: Page[] = [];
+    nextPages.push({
+      label: "1",
+      current: page === 1,
+      data: 1,
+    });
+
+    if (count <= BUTTON_LENGTH) {
+      for (let i = 2; i <= count; i++) {
+        nextPages.push({
+          label: i.toString(),
+          current: page === i,
+          data: i,
+        });
+      }
+    } else if (page <= 4) {
+      // 1 2 3 4 5 6 7
+      for (let i = 2; i <= 5; i++) {
+        nextPages.push({
+          label: i.toString(),
+          current: page === i,
+          data: i,
+        });
+      }
+      nextPages.push({
+        label: "...",
+        current: false,
+      });
+    } else if (page > count - 4) {
+      // 1 ... 4 5 6 7 8
+      nextPages.push({
+        label: "...",
+        current: false,
+      });
+      for (let i = count - 4; i < count; i++) {
+        nextPages.push({
+          label: i.toString(),
+          current: page === i,
+          data: i,
+        });
+      }
+    } else {
+      // 1 ... 4 5 6  ...8
+      nextPages.push({
+        label: "...",
+        current: false,
+      });
+      nextPages.push({
+        label: (page - 1).toString(),
+        current: false,
+        data: page - 1,
+      });
+
+      nextPages.push({
+        label: page.toString(),
+        current: true,
+        data: page,
+      });
+      nextPages.push({
+        label: (page + 1).toString(),
+        current: false,
+        data: page + 1,
+      });
+
+      nextPages.push({
+        label: "...",
+        current: false,
+      });
+    }
+
+    nextPages.push({
+      label: count.toString(),
+      current: page === count,
+      data: count,
+    });
+    setPages(nextPages);
+  }, [page, count]);
+
+  const prevPage = page - 1;
+  const nextPage = page + 1;
+
   return (
     <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
       <div className="flex flex-1 items-center justify-between">
@@ -20,67 +116,38 @@ const PaginationNav: FC<PaginationNavProps> = ({ page, count, onChange }) => {
             className="isolate inline-flex -space-x-px rounded-md shadow-sm"
             aria-label="Pagination"
           >
-            <PaginationNavButton label="1" current={false} />
-            <PaginationNavButton label="1" current={true} />
-
-            <a
-              href="#"
-              className="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+            <PaginationNavButton
+              current={false}
+              round="left"
+              data={Math.max(prevPage, 1)}
+              onClick={onClickHandler}
             >
               <span className="sr-only">Previous</span>
-              {/*Heroicon name: mini/chevron-left */}
               <ChevronLeftIcon className="w-5 h-5" />
-            </a>
-            {/*Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
-            <a
-              href="#"
-              aria-current="page"
-              className="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600 focus:z-20"
-            >
-              1
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-            >
-              2
-            </a>
-            <a
-              href="#"
-              className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex"
-            >
-              3
-            </a>
-            <span className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700">
-              ...
-            </span>
-            <a
-              href="#"
-              className="relative hidden items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 md:inline-flex"
-            >
-              8
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-            >
-              9
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
-            >
-              10
-            </a>
-            <a
-              href="#"
-              className="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+            </PaginationNavButton>
+
+            {pages.map((page, index) => {
+              return (
+                <PaginationNavButton
+                  key={index}
+                  current={page.current}
+                  data={page.data}
+                  onClick={onClickHandler}
+                >
+                  {page.label}
+                </PaginationNavButton>
+              );
+            })}
+
+            <PaginationNavButton
+              current={false}
+              round="right"
+              data={Math.min(nextPage, count)}
+              onClick={onClickHandler}
             >
               <span className="sr-only">Next</span>
-
-              {/*Heroicon name: mini/chevron-right*/}
               <ChevronRightIcon className="w-5 h-5" />
-            </a>
+            </PaginationNavButton>
           </nav>
         </div>
       </div>
--- front/src/components/ui/PaginationNavButton.tsx
+++ front/src/components/ui/PaginationNavButton.tsx
@@ -1,32 +1,52 @@
 import { FC } from "react";
 
 export type PaginationNavButtonProps = {
-  label: string;
   current: boolean;
   round?: "left" | "right" | "none";
+  children: React.ReactNode;
+  data?: number;
+  onClick?: (e: React.ChangeEvent<unknown>, page: number) => void;
 };
 const PaginationNavButton: FC<PaginationNavButtonProps> = ({
-  label,
   current,
   round = "none",
+  children,
+  data,
+  onClick,
 }) => {
   //  rounded-l-md
+
+  const roundClass =
+    round === "left" ? "rounded-l-md" : round === "right" ? "rounded-r-md" : "";
+
+  const colorClass = current
+    ? "border-indigo-500 bg-indigo-50"
+    : "border-gray-300 bg-white";
+
+  const onClickHandler = (e: React.ChangeEvent<unknown>) => {
+    e.preventDefault();
+    if (onClick) {
+      onClick(e, data!);
+    }
+  };
 
   return (
     <>
       {current ? (
         <a
           href="#"
-          className={`relative items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20`}
+          onClick={onClickHandler}
+          className={`relative items-center border ${colorClass} px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 ${roundClass}`}
         >
-          {label}
+          {children}
         </a>
       ) : (
         <a
           href="#"
-          className={`relative items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20`}
+          onClick={onClickHandler}
+          className={`relative items-center border ${colorClass} px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 ${roundClass}`}
         >
-          {label}
+          {children}
         </a>
       )}
     </>
